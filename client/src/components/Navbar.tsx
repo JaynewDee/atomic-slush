f4be264f7be5616e5bdc85281567fbb7b56b4975
NuminousBlue <jdiehl2236@gmail.com>
2023-06-17 17:25:45 -0500

    initial

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,12 @@
 export default function NavbarCTA() {
   return (
-    <div className="navbar"></div>
+    <div className="navbar">
+      <a href="/">Home</a>
+      <a href="/services">Services</a>
+      <a href="/products">Products</a>
+      <a href="/about">Who We Are</a>
+      <a href="/quote">Get a Quote</a>
+    </div>
+
   );
 }
