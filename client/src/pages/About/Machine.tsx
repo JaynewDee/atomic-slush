042237cc5d2f045bd8691550ffdc90a7ab6acf15
NuminousBlue <jdiehl2236@gmail.com>
2023-06-20 02:23:05 -0500

    the machine

--- client/src/pages/About/Machine.tsx
+++ client/src/pages/About/Machine.tsx
@@ -3,10 +3,24 @@ import React from 'react'
 const Machine = () => {
     return (
         <div className="machine-section">
-            <p>{"We offer a double-headed machine with each head carrying 9 Liters (20 Liters total!) of your two chosen flavors."}</p>
-            <p>{"The rental of the double headed machine, will provide two of our in-house made pre-mixes of your choice, 75 ct-9oz cups, delivery and setup. The total of frozen beverages served  will roughly be about 65-75 9 oz. drinks. This is ideal for parties of 25-30 guest."}</p>
-            <p>{" Additional mixes may be purchased for larger parties"}</p>
+            <span>~</span>
+            <p>
+                {"We offer a double-headed machine with each head carrying 9 Liters (20 Liters total!) of your two chosen flavors."}
+            </p>
+            <span>~</span>
 
+            <p>
+                {"The rental of the double headed machine includes two of our in-house batched mixes of your choice, 75 ct-9oz cups, delivery and setup."}
+            </p>
+            <span>~</span>
+            <p>
+                {"The total of frozen beverages served will be roughly 70 9oz. drinks. This is ideal for parties of about 25 to 30 guests."}
+            </p>
+            <span>~</span>
+            <p>
+                {" Additional mixes may be purchased for larger parties."}
+            </p>
+            <span>~</span>
         </div>
     )
 }
