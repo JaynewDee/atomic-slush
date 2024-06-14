0c90c480a984ffe9a5bba534ad4ba748900ee0d3
NuminousBlue <jdiehl2236@gmail.com>
2024-06-13 20:47:04 -0500

    refactor welcome page

--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -14,25 +14,29 @@ export default function Welcome({ setActiveView }: WelcomeProps) {
       <header className="page-header" style={{ margin: "2rem auto 3rem" }}>
         <h3>WELCOME</h3>
       </header>
-      <section
-        className="welcome-section"
+      <WelcomeSection
+        headerText="VIEW PRICING MENU"
         onClick={() => {
-          setActiveView("menu");
-          navigate("/menu");
-        }}
-      >
-        <h4 className="welcome-section-header">VIEW PRICING MENU</h4>
-      </section>
-      <section
-        className="welcome-section"
+          setActiveView('menu');
+          navigate('/menu');
+        }} />
+      <WelcomeSection
+        headerText="GET A QUOTE"
         onClick={() => {
-          setActiveView("quote");
-          navigate("/quote");
+          setActiveView('quote');
+          navigate('/quote');
+        }} />
+      <WelcomeSection
+        headerText="BOOK NOW"
+        onClick={() => {
+          window.open(
+            "https://portal.tripleseat.com/direct_bookings/qnbd70wegy3",
+            "_blank",
+          );
         }}
-      >
-        <h4 className="welcome-section-header">GET A QUOTE</h4>
-      </section>
-      <section
+        className="welcome-section book-now-section"
+      />
+      {/* <section
         className="welcome-section book-now-section"
         onClick={() => {
           window.open(
@@ -42,8 +46,23 @@ export default function Welcome({ setActiveView }: WelcomeProps) {
         }}
       >
         <h4 className="welcome-section-header">BOOK NOW</h4>
-      </section>
+      </section> */}
       <ImageCarousel />
     </article>
   );
+}
+
+const WelcomeSection = ({ headerText, onClick, className }:
+  {
+    headerText: string,
+    onClick: () => void,
+    className?: string
+  }) => {
+  return <section
+    className={className ? className : "welcome-section"}
+    onClick={onClick}
+  >
+    <h4 className="welcome-section-header">{headerText}</h4>
+  </section>
+
 }
